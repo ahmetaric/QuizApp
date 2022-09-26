ee96d13b0314973cd2b93ffbb4d9c2b2b5569ebd
ahmetaric <ahmet.aric1965@gmail.com>
2022-09-26 20:33:13 +0300

    V-1

--- quiz/models.py
+++ quiz/models.py
@@ -1,21 +1,53 @@
-from tabnanny import verbose
-from turtle import title
 from unicodedata import category
 from django.db import models
 
-# Create your models here.
+class UpdateCreateDate(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now = True)
+
+    class Meta:
+        abstract = True
 class Category(models.Model):
-    name=models.CharField(max_length=50,verbose_name="Category Name")
+    name = models.CharField(max_length=50, verbose_name='Category Name')
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name_plural = "Categories"    
+        verbose_name_plural = 'Categories'
 
-class Quiz(models.Model):
-    title = models.CharField(max_length=50,verbose_name="Quiz Title")
-    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+class Quiz(UpdateCreateDate):
+    title = models.CharField(max_length=50, verbose_name = 'Quiz Title')
+    category = models.ForeignKey(Category, on_delete = models.CASCADE)
+    #? delete questions when category changes (models.CASCADE) 👆
     
+
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name_plural = 'Quizzes'
+
+class Question(UpdateCreateDate):
+    SCALE = (
+        ('B', 'Beginner'),
+        ('I', 'Intermadiate'),
+        ('A', 'Advanced')
+    )
+    title = models.TextField()
+    quiz = models.ForeignKey(Quiz, on_delete = models.CASCADE)
+    difficulty = models.CharField(max_length = 1, choices = SCALE)
+    
+
+    def __str__(self):
+        return self.title
+
+class Option(UpdateCreateDate):
+    option_text = models.CharField(max_length = 200)
+    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+    is_right = models.BooleanField(default=False)
+    
+
+    def __str__(self):
+        return self.option_text
+    
